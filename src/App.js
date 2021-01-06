41cd11f020a1747e44fdbba3c3b9ad58384261a7
Pavel Tishchuk <pavel.tishchyk@gmail.com>
2021-01-06 21:22:05 +0200

    Upload test task files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const { default: MaterialTable } = require("material-table")
+const data = require("./data/users.json")
+
+const App = () => {
+  const [users, setUsers] = useState(null);
+
+  const [columns] = useState([
+    { title: 'Username', field: 'username' },
+    { title: 'Name', field: 'name' },
+    { title: 'Email', field: 'email' },
+    { title: 'Phone', field: 'phone' },
+    { title: 'Website', field: 'website' },
+  ]);
+
+  useEffect(() => {
+    if(!users) {
+      new Promise((resolve, reject) => {
+        setTimeout(() => resolve(data), 2000)
+      }).then(users => setUsers(users))
+    }
+  }, [users])
+
+  const onRowAdd = newData => (
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        setUsers([...users, newData]);
+        
+        resolve();
+      }, 1000)
+    })
+  )
+            
+  const onRowUpdate = (newData, oldData) => (
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const dataUpdate = [...users];
+        const index = oldData.tableData.id;
+        dataUpdate[index] = newData;
+        setUsers([...dataUpdate]);
+
+        resolve();
+      }, 1000)
+    })
+  )
+            
+  const onRowDelete = oldData => (
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const dataDelete = [...users];
+        const index = oldData.tableData.id;
+        dataDelete.splice(index, 1);
+        setUsers([...dataDelete]);
+        
+        resolve()
+      }, 1000)
+    })
+  )
+            
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <MaterialTable
+        title="Users"
+        isLoading={!users}
+        columns={columns}
+        data={users ? users : []}
+        editable={{
+          onRowAdd,
+          onRowUpdate,
+          onRowDelete,
+        }}
+      />
     </div>
   );
 }
